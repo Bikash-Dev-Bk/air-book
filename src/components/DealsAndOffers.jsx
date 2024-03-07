55108c3cfaf7cf2fea8ee60847af4329364844be
bikash-dev <bikash.dev.bk@gmail.com>
2024-03-07 12:03:33 +0600

    DealsAndOffers update

--- src/components/DealsAndOffers.jsx
+++ src/components/DealsAndOffers.jsx
@@ -5,37 +5,37 @@ import img2 from "../assets/images/Offer/sliderUp_to_14__discount_on_Domestic_fl
 import img3 from "../assets/images/Offer/sliderUp_to_14__discount_on_Domestic_flights 2.png";
 
 const DealsAndOffers = () => {
+  const deals = [
+    {
+      image: img3,
+      title: "With bkash payment only",
+      offer: "up to 14% discount on the base fare of domestic flights",
+    },
+    {
+      image: img1,
+      title: "With bkash payment only",
+      offer: "up to 14% discount on the base fare of domestic flights",
+    },
+    {
+      image: img2,
+      title: "With bkash payment only",
+      offer: "up to 14% discount on the base fare of domestic flights",
+    },
+  ];
+
   return (
     <div className="container">
       <h1 className="section_title">Deals And Offers</h1>
       <div className="offers_container">
-        <div className="offers_items">
-          <img src={img3} alt="" />
-          <div>
-            <p className="offers_item_title">With bkash payment only</p>
-            <h2 className="offers_item_discount">
-              up to 14% discount on the base fare of domestic flights
-            </h2>
+        {deals.map((deal, index) => (
+          <div key={index} className="offers_items">
+            <img src={deal.image} alt="" />
+            <div>
+              <p className="offers_item_title">{deal.title}</p>
+              <h2 className="offers_item_discount">{deal.offer}</h2>
+            </div>
           </div>
-        </div>
-        <div className="offers_items">
-          <img src={img1} alt="" />
-          <div>
-            <p className="offers_item_title">With bkash payment only</p>
-            <h2 className="offers_item_discount">
-              up to 14% discount on the base fare of domestic flights
-            </h2>
-          </div>
-        </div>
-        <div className="offers_items">
-          <img src={img2} alt="" />
-          <div>
-            <p className="offers_item_title">With bkash payment only</p>
-            <h2 className="offers_item_discount">
-              up to 14% discount on the base fare of domestic flights
-            </h2>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
