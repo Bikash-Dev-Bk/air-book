adf11786337d4f17a9a3a8830502e2aa5be060ef
bikash-dev <bikash.dev.bk@gmail.com>
2024-03-07 16:54:00 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
-
+import "./styles/Navbar.css";
+import "./styles/Container.css";
+import logo1 from "../assets/images/Logo/LOGO.png";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
+  return (
+    <div className="nav_container">
+      <div className="container">
+        <div className="content">
+          <img src={logo1} alt="" />
+          <div className="nav_routes">
+            <p>Blog</p>
+            <p>Offer</p>
+            <p>Airlines</p>
+            <p>About</p>
+          </div>
+          <button className="sign_btn">Sign in</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
