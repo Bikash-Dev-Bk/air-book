0f44141926eb734e675b524655429f942f139065
bikash-dev <bikash.dev.bk@gmail.com>
2024-03-07 13:29:50 +0600

    newsletter added

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,10 +1,23 @@
+import "./styles/Newsletter.css";
+import "./styles/Container.css";
+import newsletter from "../assets/images/News letter/Isolation_Mode.png";
 
 const Newsletter = () => {
-    return (
-        <div>
-            <h1>Newsletter</h1>
+  return (
+    <div className="container">
+      <div className="newsletter_container">
+        <div className="newsletter_half">
+          <img src={newsletter} alt="" className="newsletter_image" />
         </div>
-    );
+        <div className="newsletter_half">
+          <h1 className="newsletter_title">Subscribe to our Newsletter!</h1>
+          <p className="newsletter_tagline">Subscribe to our newsletter and stay updated.</p>
+          <input type="text" className="newsletter_input" placeholder="Your Email" />
+          <button className="subscribe_btn">Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
